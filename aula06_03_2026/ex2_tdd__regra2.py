76cfccf1e3045f934a8523477cfa9901465bcfae
Alessandra F Rigonatti <alessandra.rigonatti@aluno.faculdadeimpacta.com.br>
2026-03-06 14:48:48 -0300

    fase 2 tdd

--- aula06_03_2026/ex2_tdd__regra2.py
+++ aula06_03_2026/ex2_tdd__regra2.py
@@ -1,9 +1,18 @@
 
 def termometrica (tipo_temperatura, temperatura):
 
-    if tipo_temperatura == "Cc":
-        return
+    """função quer recebe entrada em um tipo de grau de temperatura e converte para outra"""
+    
+    # atenção isdigt() é para string
+    if isinstance(temperatura, (int, float)):
+        if tipo_temperatura.lower() == "c":
+            # arrendondar valor para 2 casas
+            fahe = (9/5)*temperatura+32
+            return round(fahe, 2)
+        else:
+            if tipo_temperatura.lower() == "f":
+                celso = (5/9)*(temperatura-32)
+                return round(celso, 2)
     else:
-        if tipo_temperatura == "Ff":
-            return
-   
+        raise ValueError ("Temperaura deve ser apenas valor numérico")
+    
